92b0dffea3e95260a17d6f0d10d68771b0fdd7bf
israel <israelpadonu14@gmail.com>
2024-10-04 19:35:30 +0100

    .

--- src/http/Api.js
+++ src/http/Api.js
@@ -8,24 +8,27 @@ const supabaseKey = process.env.SUPABASE_KEY
 const supabase = createClient(supabaseUrl, supabaseKey)
 
 const PORT = 8000
-
+var corsOptions = {
+    origin: 'http://localhost:5173',
+    optionsSuccessStatus: 200 // some legacy browsers (IE11, various SmartTVs) choke on 204
+}
 export const MESSAGE_PATH = "/message"
 // Setup HTTP api
 const api = express()
 api.use(express.json())
-api.use(cors())
+api.use(cors(corsOptions))
 api.use(bodyParser.json());
 
 api.post('/user', async (req, res) => {
     const { telegramId } = req.body;
-
+    
     if (!telegramId) {
         return res.status(400).json({ error: 'Telegram ID is required' });
     }
 
     try {
         // Check if the user already exists
-        let existingUser = await prisma.user.findUnique({ where: { telegramId } });
+        let existingUser = await prisma.user.findUnique({ where: { telegramId: telegramId } })
         if (existingUser) {
             return res.status(400).json({ error: 'User already exists' });
         }
@@ -38,16 +41,16 @@ api.post('/user', async (req, res) => {
             }
         });
 
-       return res.status(201).json({ message: 'User created successfully', user: newUser });
+        return res.status(201).json({ message: 'User created successfully', user: newUser });
     } catch (error) {
-        res.status(500).json({ error: 'Server error' });
+        res.status(500).json({ error: 'Server error', msg: error });
     }
 });
 
 
 
 // Listen to server start on port
- api.listen(PORT, () => console.log(`express is up on port ${PORT}`))
+api.listen(PORT, () => console.log(`express is up on port ${PORT}`))
 
 
 
