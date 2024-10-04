6501ef6309e4dd6798fe55e412cd12dad4276fb7
israel <israelpadonu14@gmail.com>
2024-10-04 17:30:10 +0100

    .

--- src/http/Api.js
+++ src/http/Api.js
@@ -2,7 +2,7 @@ import cors from 'cors';
 import express from 'express';
 import bodyParser from 'body-parser';
 import { createClient } from '@supabase/supabase-js'
-
+import { prisma } from './prisma.js';
 const supabaseUrl = 'https://ljtjkdhtecpvzbdlzjbi.supabase.co'
 const supabaseKey = process.env.SUPABASE_KEY
 const supabase = createClient(supabaseUrl, supabaseKey)
@@ -10,20 +10,52 @@ const supabase = createClient(supabaseUrl, supabaseKey)
 const PORT = 8000
 
 export const MESSAGE_PATH = "/message"
+// Setup HTTP api
+const api = express()
+api.use(express.json())
+api.use(cors())
+api.use(bodyParser.json());
 
-export  function launchApi() {
-    // Setup HTTP api
-    const api = express()
-    api.use(express.json())
-    api.use(cors())
-    api.use(bodyParser.json());
-   
-      
-    // Listen to server start on port
-    api.listen(PORT, () => console.log(`express is up on port ${PORT}`))
+api.post('/user', async (req, res) => {
+    const { telegramId } = req.body;
 
-    return api  
-} 
+    if (!telegramId) {
+        return res.status(400).json({ error: 'Telegram ID is required' });
+    }
+
+    try {
+        // Check if the user already exists
+        let existingUser = await prisma.user.findUnique({ where: { telegramId } });
+        if (existingUser) {
+            return res.status(400).json({ error: 'User already exists' });
+        }
+
+        // Create a new user
+        const newUser = await prisma.user.create({
+            data: {
+                telegramId,
+                points: 0
+            }
+        });
+
+       return res.status(201).json({ message: 'User created successfully', user: newUser });
+    } catch (error) {
+        res.status(500).json({ error: 'Server error' });
+    }
+});
+
+
+
+// Listen to server start on port
+ api.listen(PORT, () => console.log(`express is up on port ${PORT}`))
+
+
+
+export function launchApi() {
+
+
+    return api
+}
 
 
 
--- src/index.js
+++ src/index.js
@@ -1,4 +1,8 @@
 import {launchApp} from "./app/Application.js";
-
+import cors from 'cors';
+import express from 'express';
+import bodyParser from 'body-parser';
+import { createClient } from '@supabase/supabase-js'
+import { prisma } from './http/prisma.js';
 // Launch our application
-launchApp()
+ launchApp()
