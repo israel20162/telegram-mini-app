3e2e304a04512de004a4a993aa89f0095e108d4c
israel <israelpadonu14@gmail.com>
2024-10-05 12:41:18 +0100

    .

--- src/http/prisma.js
+++ src/http/prisma.js
@@ -1,4 +1,6 @@
-const { PrismaClient } = require('@prisma/client')
 
-export const prisma = new PrismaClient()
-// use `prisma` in your application to read and write data in your DB
+
+
+
+import { PrismaClient } from '@prisma/client';
+export const prisma = new PrismaClient();
