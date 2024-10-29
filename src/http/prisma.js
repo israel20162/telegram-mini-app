2dc53b59e6851dc6ec9d466743d14b6d99f32cd9
israel <israelpadonu14@gmail.com>
2024-10-29 19:58:19 +0100

    .

--- src/http/prisma.js
+++ src/http/prisma.js
@@ -1,4 +1,4 @@
 import { PrismaClient } from "@prisma/client";
 // const PrismaClient = require('@prisma/client')
-export const prisma = new PrismaClient()
+export default  prisma = new PrismaClient()
 
